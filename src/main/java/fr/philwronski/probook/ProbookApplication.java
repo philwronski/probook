cc0f6e345e8309404a9c0cd7d1a6f16ef4276435
Philippe Wronski <philippe.wronski@gmail.com>
2017-11-10 09:58:45 +0100

    Initialize project

---
+++ src/main/java/fr/philwronski/probook/ProbookApplication.java
@@ -0,0 +1,12 @@
+package fr.philwronski.probook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProbookApplication.class, args);
+	}
+}
